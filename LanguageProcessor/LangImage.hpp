45cdacb0f2bd82fc13e665f82135b1c4134d7ecc
Alan Schuppan <aschuppa@hach.com>
2017-08-01 08:51:22 -0600

    * Moved LangImage out of LanguageProcessor.cpp

---
+++ LanguageProcessor/LangImage.hpp
@@ -0,0 +1,42 @@
+#ifndef LANG_IMAGE_DEFD
+#define LANG_IMAGE_DEFD
+
+#include <ostream>
+#include <string>
+#include <vector>
+
+//##############################################################################
+// CLangImage
+//##############################################################################
+//! Creates a binary image, as shown in the following table, of the translations
+//! for a given language, supporting fast look-up given a string ID.
+//##############################################################################
+
+class CLangImage {
+public:
+    CLangImage();
+    CLangImage(const CLangImage &other);
+    CLangImage(CLangImage &&other);
+    ~CLangImage();
+
+    void Fill(const std::string &language,
+        const std::vector<std::wstring> &translations,
+        uint8_t langOrder, uint8_t charSet = 0);
+    const uint8_t *Image() const { return mpImage; }
+    const void SaveAsCode(const std::vector<std::string> &enums,
+        std::ostream &stream) const;
+    //void GenerateCode(const std::vector<std::string> &enums, std::ostream &file) const;
+    uint32_t Size() const { return mTotalSize; }
+    void Show() const;
+private:
+    std::string mLanguage;
+    uint8_t *mpImage;
+    uint8_t *mpWrite;
+    uint32_t mTotalSize;
+
+    void Write(uint32_t value, uint32_t size);
+    static void WriteLine(const uint8_t **ppimage, uint32_t maxBytes, uint32_t bytes,
+        const std::string &comment, std::ostream &file);
+};
+
+#endif // LANG_IMAGE_DEFD
